15870fa5b84837517dd0e8b0588a0693deeace77
MISBAH <misbah@MacBook-Pro-1.local>
2019-05-09 13:46:41 +0700

    Initial commit

---
+++ app/src/main/java/com/miss/splashscreen2/MainActivity.java
@@ -0,0 +1,45 @@
+package com.miss.splashscreen2;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+    //set waktu lama sPlashscreen
+    private static int LamaTampilSplash = 3000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //MULAI KODING
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        // KODING
+        setContentView(R.layout.activity_main);
+        //KODING LAGI
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // to do auto generated stub
+                Intent apasih = new Intent(MainActivity.this,Main2Activity.class);
+                startActivity(apasih);
+
+                // jeda setelah splashscren
+
+                this.selesai();
+            }
+
+            private void selesai(){
+                //auto
+            }
+        },LamaTampilSplash);
+
+    };
+    }
+
